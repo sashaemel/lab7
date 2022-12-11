26bddcbdf079593c2059227e945127c7d8bda154
sashaemel <120336512+sashaemel@users.noreply.github.com>
2022-12-11 22:52:03 +0300

    result

---
+++ script.js
@@ -0,0 +1,15 @@
+
+  var btn = document.getElementById("btn");
+btn.addEventListener("click", function() {
+  this.classList.add("active");
+});
+
+var butn = document.getElementById("butn");
+butn.addEventListener("click", function() {
+  this.classList.add("active");
+});
+
+var buttn = document.getElementById("buttn");
+buttn.addEventListener("click", function() {
+  this.classList.add("active");
+});
